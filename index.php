c779983c368c931a9d42e29388fc9cb54f68b96c
Ederson <deson00@gmail.com>
2017-12-07 13:25:56 -0300

    Arquivo index.ph e index.html desnecessario da raiz deletado

--- index.php
+++ index.php
@@ -1,11 +1,37 @@
-<?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/site/login');
-	exit;
-?>
-Something is wrong with the XAMPP installation :-(
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Ola!</title>
+  <link href="/site/style.css" rel="stylesheet" type="text/css" media="all" />
+</head>
+
+<body>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<table width="0" height="0" border="0" align="center">
+  <tr>
+    <td><img src="/site/images/chave.jpg" alt="cadeado" width="102" height="102" /></td>
+    <td>Voc&ecirc; precisa estar logado para acessar esta pagina.</td>
+    <td></td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td><form action="/site/login/validacao.php" method="post">
+      <fieldset>
+      <legend>Dados de Login</legend>
+        <label for="txUsuario">Usuario</label>
+      <input type="text" name="usuario" id="txUsuario" maxlength="25" />
+      <label for="txSenha">Senha</label>
+      <input type="password" name="senha" id="txSenha" />
+      <input name="submit" type="submit" value="Entrar" />
+      </fieldset>
+    </form></td>
+    <td>&nbsp;</td>
+  </tr>
+ 
+</table>
+</body>
+
+</html>
