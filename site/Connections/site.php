10a26c6bfc2e2a8c5368f0e6ef877a4ebf88cfeb
Bylltec <31113279+Bylltec@users.noreply.github.com>
2019-09-05 17:01:42 -0400

    adicionado modo de conexão
    adicionado modo de conexao mysql

--- site/Connections/site.php
+++ site/Connections/site.php
@@ -7,4 +7,9 @@ $database_site = "Syslog";
 $username_site = "root";
 $password_site = "123456";
 $site = mysql_pconnect($hostname_site, $username_site, $password_site) or trigger_error(mysql_error(),E_USER_ERROR); 
+$mysqli = new mysqli($hostname_site, $username_site, $password_site, $database_site);
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+} 
+
 ?>
