f9299049a537dc191ef7a9f4b2f4e042cf45bf08
Bylltec <31113279+Bylltec@users.noreply.github.com>
2019-09-17 16:39:39 -0400

    modificado caminho par remover arquivos de backups

--- site/gestorserver/log/backuplog/action_exclui_arquivo.php
+++ site/gestorserver/log/backuplog/action_exclui_arquivo.php
@@ -1,24 +1,33 @@
 <?php
-// A sess�o precisa ser iniciada em cada p�gina diferente
+// A sessï¿½o precisa ser iniciada em cada pï¿½gina diferente
 if (!isset($_SESSION)) session_start();
 $nivel_necessario = 5;
-// Verifica se n�o h� a vari�vel da sess�o que identifica o usu�rio
+// Verifica se nï¿½o hï¿½ a variï¿½vel da sessï¿½o que identifica o usuï¿½rio
 if (!isset($_SESSION['UsuarioID']) OR ($_SESSION['UsuarioNivel'] > $nivel_necessario)) {
-	// Destr�i a sess�o por seguran�a
+	// Destrï¿½i a sessï¿½o por seguranï¿½a
 	session_destroy();
 	// Redireciona o visitante de volta pro login
 	header("Location: /site/login/index.php"); exit;
 }
 ?>
 <?php
-$arquivoNome = $_REQUEST["arquivo"];
-if(unlink($arquivoNome)){
-	echo "Arquivo excluido com sucesso.";
-}
-else{
-	echo "Erro, ontate o administrador";
+$arquivoNome = "/var/backups/gestorlog/".$_REQUEST["arquivo"];
+
+$extensao = substr($arquivoNome, -6); 
+if($extensao == "tar.gz" or $extensao == "TAR.GZ"){
+
+	if(unlink($arquivoNome)){
+		echo "Arquivo excluido com sucesso.";
+	}
+	else{
+		echo "Erro, Contate o administrador 1";
+		exit;
+	}
+}else{
+	echo "Erro, Contate o administrador 2";
 	exit;
+
 }
-echo "<script type='text/javascript'> alert('Arquivo excluido corretamente e espaço de armazenamento liberado.');</script>"; 
+echo "<script type='text/javascript'> alert('Arquivo excluido corretamente e espaÃ§o de armazenamento liberado.');</script>"; 
 echo '<head><meta http-equiv=refresh content="0; URL=/site/gestorserver/log/?pagina=status"></head>';
 ?>
