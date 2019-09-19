3b0fd7e897a86dda73235413a17d1f880323d4a9
Bylltec <31113279+Bylltec@users.noreply.github.com>
2019-09-19 17:11:14 -0400

    retirado algumas linhas desnecessárias

--- site/gestorserver/log/backuplog/action_backup.php
+++ site/gestorserver/log/backuplog/action_backup.php
@@ -14,28 +14,12 @@ if (!isset($_SESSION['UsuarioID']) OR ($_SESSION['UsuarioNivel'] > $nivel_necess
 require_once'../../../Connections/site.php';
 mysql_select_db($database_site, $site);
 $data_hora = date("YmdHis");
-
 $comando = "mysqldump -u $username_site -p$password_site Syslog > /var/backups/gestorlog/diario/$data_hora.sql";
 //echo $comando;
-
 $output = shell_exec($comando);
 //echo $output;
-
-//echo $comando_compactar = "cd /var/www/html/Gestor-Log-Mikrotik-Linux/site/gestorserver/log/backuplog/ ;/bin/tar -czvf mysql.bkp_data_hora.tar.gz data_hor$
-
-//$comando_compactar = ('ls -lart');
 $saida_comando_compactar = shell_exec("cd /var/backups/gestorlog/diario/;
  /bin/tar -czvf $data_hora.tar.gz $data_hora.sql; rm $data_hora.sql");
-
-//echo "<pre>$saida_comando_compactar</pre>";
-//echo $saida_comando_compactar;
-/*
-
-//exit;
-//$output = shell_exec('ls -lart');
-echo "<pre>$output</pre>";
-*/
-echo "<script type='text/javascript'> alert('Backup realizado com data e hora atual confira na lista no menu status do servidor o novo arquivo criado.');</s$
-echo '<head><meta http-equiv=refresh content="0; URL=/site/gestorserver/log/?pagina=status"></head>';
-
 ?>
+<script type='text/javascript'> alert('Backup realizado com data e hora atual confira na lista no menu status do servidor o novo arquivo criado.');</script>
+<head><meta http-equiv=refresh content="0; URL=/site/gestorserver/log/?pagina=status"></head>
