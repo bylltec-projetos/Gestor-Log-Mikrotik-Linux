752c0b16c8721a9c2a7adf85b4152e48c3303635
Bylltec <31113279+Bylltec@users.noreply.github.com>
2019-09-19 10:01:43 -0400

    adicionado listar arquivos da ultimas horas
    adicionado listagem de arquivo de backup das ultimas horas

--- site/gestorserver/log/backuplog/listar_arquivos.php
+++ site/gestorserver/log/backuplog/listar_arquivos.php
@@ -1,10 +1,10 @@
 <?php
-// A sess�o precisa ser iniciada em cada p�gina diferente
+// A sessï¿½o precisa ser iniciada em cada pï¿½gina diferente
 if (!isset($_SESSION)) session_start();
 $nivel_necessario = 5;
-// Verifica se n�o h� a vari�vel da sess�o que identifica o usu�rio
+// Verifica se nï¿½o hï¿½ a variï¿½vel da sessï¿½o que identifica o usuï¿½rio
 if (!isset($_SESSION['UsuarioID']) OR ($_SESSION['UsuarioNivel'] > $nivel_necessario)) {
-	// Destr�i a sess�o por seguran�a
+	// Destrï¿½i a sessï¿½o por seguranï¿½a
 	session_destroy();
 	// Redireciona o visitante de volta pro login
 	header("Location: /site/login/index.php"); exit;
@@ -31,7 +31,7 @@ function tamanho_arquivo($arquivo) {
 ?>
 <table class="table">
 <tr>
-<th colspan="4">Manutenção dos Backups</th>
+<th colspan="4">ManutenÃ§Ã£o dos Backups</th>
 </tr>
 <tr>
 <td>
@@ -44,9 +44,7 @@ function tamanho_arquivo($arquivo) {
 <?php
 	//echo 'GLOB'.PHP_EOL;
 	//chdir( '.' );
-	//caminho onde esta armazenado o backup compactado
 	//chdir( '/var/www/html/Gestor-Log-Mikrotik-Linux/site/gestorserver/log/backuplog/' );
-	//caminho dos backups
 	chdir( '/var/backups/gestorlog/' );
 	$arquivos = glob("{*.txt,*.sql,*tar.gz}", GLOB_BRACE);// aqui vai os tipos de arquivos por extensao a ser listado separado por virgula
 	sort($arquivos);
@@ -91,3 +89,57 @@ function tamanho_arquivo($arquivo) {
 
 
 </table>
+
+<table class="table">
+<tr>
+<th colspan="4">Backups das ultimas horas</th>
+</tr>
+<?php
+	//echo 'GLOB'.PHP_EOL;
+	//chdir( '.' );
+	//chdir( '/var/www/html/Gestor-Log-Mikrotik-Linux/site/gestorserver/log/backuplog/' );
+	chdir( '/var/backups/gestorlog/diario/' );
+	$arquivos = glob("{*.txt,*.sql,*tar.gz}", GLOB_BRACE);// aqui vai os tipos de arquivos por extensao a ser listado separado por virgula
+	sort($arquivos);
+	foreach($arquivos as $img){
+		?>
+		<tr>
+		<td><?php echo $img; ?></td>
+		<td><?php echo tamanho_arquivo($img); ?></td>
+<td><a href="/site/gestorserver/log/backuplog/action_download_arquivo.php?arquivo=<?php echo $img; ?>" class="btn btn-primary btn-sm">Baixar</a></td>
+<td><a href="/site/gestorserver/log/backuplog/action_exclui_arquivo.php?arquivo=<?php echo $img; ?>" class="btn btn-danger btn-sm">Excluir</a></td>
+</tr>
+<?php
+	} 
+		
+	//echo PHP_EOL;
+
+
+
+/*
+	$types = array( 'php', 'jpg' );
+	
+    echo 'DIR FUNCTIONS'.PHP_EOL;
+	if ( $handle = opendir('.') ) {
+	    while ( $entry = readdir( $handle ) ) {
+	    	$ext = strtolower( pathinfo( $entry, PATHINFO_EXTENSION) );
+			if( in_array( $ext, $types ) ) echo $entry.PHP_EOL;
+	    }
+	    closedir($handle);
+	}    
+	echo PHP_EOL;
+
+    echo 'DIRECTORY ITERATOR'.PHP_EOL;
+	$path = '.';
+	$dir = new DirectoryIterator($path);
+	foreach ($dir as $fileInfo) {
+    	$ext = strtolower( $fileInfo->getExtension() );
+	    if( in_array( $ext, $types ) ) echo $fileInfo->getFilename().PHP_EOL;
+	}    
+*/
+
+	?>
+
+
+</table>
+
