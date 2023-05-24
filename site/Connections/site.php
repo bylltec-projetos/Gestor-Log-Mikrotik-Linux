1913ef49b881196fa663315db792be64dcb4e03f
Ederson Pereira de Brito <deson00@gmail.com>
2023-05-24 16:59:15 -0400

    adicionado conexao pdo

--- site/Connections/site.php
+++ site/Connections/site.php
@@ -1,15 +1,14 @@
 <?php
-# FileName="Connection_php_mysql.htm"
-# Type="MYSQL"
-# HTTP="true"
 $hostname_site = "localhost";
 $database_site = "Syslog";
 $username_site = "root";
-$password_site = "123456";
-$site = mysql_pconnect($hostname_site, $username_site, $password_site) or trigger_error(mysql_error(),E_USER_ERROR); 
-$mysqli = new mysqli($hostname_site, $username_site, $password_site, $database_site);
-if ($mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-} 
+$password_site = "";
 
+try {
+    $pdo = new PDO("mysql:host=$hostname_site;dbname=$database_site", $username_site, $password_site);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Failed to connect to MySQL: " . $e->getMessage();
+    exit();
+}
 ?>
